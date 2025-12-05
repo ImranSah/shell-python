7048a118e37a82b66f375f5befa48da7faad775f
ImranSah <imransah1623@gmail.com>
2025-12-05 21:22:10 +0530

    implemented taking user command

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 import sys
 
+INVALID_MSG = "command not found"
+
+def printMsg(arg):
+    print(f'{arg}: {INVALID_MSG}')
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    cmdInput = input()
+
+    printMsg(cmdInput)
+
     pass
 
 
