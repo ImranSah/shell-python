065a6ba28bf1f700a21d8d54b2c32c8548831794
ImranSah <imransah1623@gmail.com>
2025-12-05 23:34:56 +0530

    implemented REPL loop

--- app/main.py
+++ app/main.py
@@ -2,17 +2,23 @@ import sys
 
 INVALID_MSG = "command not found"
 
-def printMsg(arg):
-    print(f'{arg}: {INVALID_MSG}')
+def evalute(cmd):
+    match cmd:
+        case "exit":
+            sys.exit()
+        case default:
+            return f'{cmd}: {INVALID_MSG}'
+
+def replLoop():
+    sys.stdout.write("$ ")
+    cmd = input().strip()
+    result = evalute(cmd)
+    print(result)
+
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    cmdInput = input().strip()
-
-    printMsg(cmdInput)
-
-    pass
+    while(True):
+        replLoop()
 
 
 if __name__ == "__main__":
