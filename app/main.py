786e69edca44bc0e0feb8174c67900cabb9b210f
ImranSah <imransah1623@gmail.com>
2025-12-06 23:27:33 +0530

    refactor type command

--- app/main.py
+++ app/main.py
@@ -1,37 +1,48 @@
+import os
 import sys
 
 INVALID_MSG = "command not found"
 NOTFOUND = "not found"
+BUILTINS_LIST = ["exit", "echo", "type"]
 
-BUILTINS = {
-    "exit": lambda *_: sys.exit(),
-    "echo": lambda *args: print(" ".join(args)),
-    "type" : lambda *args: print(f'{" ".join(args)} is a shell builtin') if len(args)>0 and args[0] in BUILTINS else print(f'{" ".join(args)}: {NOTFOUND}'),
-    "invalidCmd":lambda *args: print(f'{" ".join(args)}: {NOTFOUND}')
-}
-
+# Split the command line input into command and arguments
 def commandIter(commandLineInput):
     if " " in commandLineInput:
         return commandLineInput.split(" ",1)
     else:
         return [commandLineInput,""]
 
+# Evaluate the command and return the result
 def evalute(cmd, rest):
     match cmd:
-        case default:
-            return f'{cmd}: {INVALID_MSG}'
+        case "exit":
+            sys.exit()
+        case "echo":
+            return f'{rest}'
+        case "type":
+            if rest in BUILTINS_LIST:
+                return f'{rest} is a shell builtin'
+            else:
+                for d in os.environ["PATH"].split(os.pathsep):
+                    p = os.path.join(d, rest)
+                    if os.access(p, os.X_OK):
+                        return f"{rest} is {p}"
 
+                # if not found, return not found
+                return f'{rest}: {NOTFOUND}'
+        case _:
+            return f'{rest}: {INVALID_MSG}'
+
+# Main loop for the shell
 def replLoop():
     sys.stdout.write("$ ")
-    commandLineInput = input().strip()
-    cmd, rest = commandIter(commandLineInput)
-    if cmd in BUILTINS:
-        BUILTINS[cmd](rest)
-    else:
-        result = evalute(cmd, rest)
-        print(result)
+    user_input = input().strip()
+    cmd, rest = commandIter(user_input)
+    result = evalute(cmd, rest)
+    print(result)
 
 
+# Main function to run the shell
 def main():
     while(True):
         replLoop()
